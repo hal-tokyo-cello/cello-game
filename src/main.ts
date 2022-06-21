4dcc07729e7c115513814f31e840a9a4314df63c
sean-dp <daikiahal902@outlook.jp>
2022-06-21 17:15:11 +0900

    add Login and Signup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-
 import App from "./App.vue";
 import Index from "./views/Index.vue";
 import Login from "./views/Login.vue"
+import "primevue/resources/themes/fluent-light/theme.css"
+import "primevue/resources/primevue.min.css"
+import "primeicons/primeicons.css"
+//PrimeVue Import
+import InputText from 'primevue/inputtext';
+import Password from 'primevue/password';
+import Button from 'primevue/button';
+
 
 
 const routes: RouteRecordRaw[] = [
@@ -16,4 +23,8 @@ const router = createRouter({
   routes: routes,
 });
 
-createApp(App).use(router).use(PrimeVue).mount("#app");
+const app = createApp(App)
+app.use(router).use(PrimeVue).mount("#app");
+app.component("InputText",InputText);
+app.component("Password",Password);
+app.component("Button",Button);
---
+++ src/views/Signup.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        <h1>ログイン</h1>
+        <span class="p-float-label">
+	        <InputText id="username" type="text" v-model="value" />
+	        <label for="username">メールアドレス</label>
+        </span> 
+        <span class="p-float-label">
+            <Password id="password" v-model="value11" />
+            <label for="password">パスワード</label>
+        </span>
+        <span class="p-float-label">
+            <Password id="password" v-model="value11">
+            <label for="password">確認用パスワード</label>
+        </span>
+    </main>
+</template>
+<script>
+export default {
+}
+</script>
