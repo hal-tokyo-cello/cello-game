8024e4988c32c70b4e0d3850a93664bdfb5dac1e
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-06-26 17:59:45 +0000

    feat: API問い合わせ用のヘルパー関数を実装

---
+++ src/util/api.ts
@@ -0,0 +1,39 @@
+/**
+ * APIサーバーのホストのベースURL。
+ */
+export const ServerHost = "";
+
+/**
+ * API問い合わせ用のfetchヘルパー関数。
+ * @param endPoint APIエンドのルート
+ * @param method リクエストメソッド
+ * @param body リクエストのbody部
+ * @param respondValidator レスポンスのバリデータ。検証成功の場合はtrue、失敗の場合はfalseか、出力するメッセージをreturn。
+ * @returns APIからのレスポンス
+ */
+export const accessApi = <T, U>(
+  endPoint: string,
+  method: "GET" | "POST",
+  body: T,
+  respondValidator?: (res: Response) => boolean | string
+): Promise<U> => {
+  return fetch(`${ServerHost}/${endPoint}`, {
+    body: JSON.stringify(body),
+    method: method,
+  }).then((res) => {
+    if (respondValidator === undefined) {
+    if (res.status != 200) {
+        return Promise.reject(res.statusText);
+    }
+    } else {
+    const valid = respondValidator(res);
+      if (typeof valid === "string") {
+        return Promise.reject(valid);
+      } else if (!valid) {
+        return Promise.reject(res.statusText);
+      }
+    }
+
+    return res.json();
+  });
+};
