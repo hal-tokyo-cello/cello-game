4afb513a5b8206e2acc2705abb6032cbf19a0a09
KAIN <kain.nishida@gmail.com>
2022-06-28 16:34:06 +0900

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import "primevue/resources/themes/fluent-light/theme.css"
-import "primevue/resources/primevue.min.css"
-import "primeicons/primeicons.css"
+import "primevue/resources/themes/fluent-light/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
+import Mypage from "./views/Mypage.vue";
+
+//PrimeVue Import
+import Button from "primevue/button";
 
 import App from "./App.vue";
 
-const routes: RouteRecordRaw[] = [];
+const routes: RouteRecordRaw[] = [
+  { path: "/mypage", name: "mypage", component: Mypage },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: routes,
 });
 
-const app = createApp(App)
+const app = createApp(App);
 app.use(router).use(PrimeVue).mount("#app");
+app.component("Button", Button);
---
+++ src/views/Mypage.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="content">
+    <div class="top">
+      <h1>マイページ</h1>
+    </div>
+
+    <div class="box">
+      <div class="image">
+        <Avatar
+          image="/src/assets/img/kimawari.png"
+          class="mr-2"
+          size="xlarge"
+          shape="circle"
+        />
+      </div>
+
+      <div class="change">
+        <div class="username">
+          <p>{{ username }}</p>
+          <Button label="変更する" class="btn" />
+        </div>
+        <div class="mail">
+          <p>E-mail：{{ mail }}</p>
+          <Button label="変更する" class="btn" />
+        </div>
+        <div class="password">
+          <p>password</p>
+          <Button label="変更する" class="btn" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from "primevue/avatar";
+export default {
+  data() {
+    return {
+      username: "西田果韻",
+      mail: "sepatakuro@gmail.com",
+    };
+  },
+  methods: {},
+  components: {
+    Avatar,
+  },
+};
+</script>
+
+<style>
+.content {
+  top: 0px;
+  left: 0px;
+  opacity: 1;
+  margin: auto;
+  width: 60%;
+}
+
+.box {
+  display: flex;
+  justify-content: space-between;
+}
+
+.username {
+  display: flex;
+  justify-content: space-between;
+}
+
+.mail {
+  display: flex;
+  justify-content: space-between;
+}
+
+.password {
+  display: flex;
+  justify-content: space-between;
+}
+
+.btn {
+  margin: 5px;
+}
+</style>
