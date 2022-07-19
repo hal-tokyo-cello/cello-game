cae1bb139882582f1cb857a35fb274e317200200
suzuki tomoya <tomsu.3288@gmail.com>
2022-07-19 14:53:23 +0900

    静的

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
 import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import "primevue/resources/themes/fluent-light/theme.css"
-import "primevue/resources/primevue.min.css"
-import "primeicons/primeicons.css"
+import "primevue/resources/themes/fluent-light/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+
 import App from "./App.vue";
+import fourchoice from "./views/fourchoice.vue";
 
-
-const routes: RouteRecordRaw[] = [];
+const routes: RouteRecordRaw[] = [
+  { path: "/fourchoice", name: "fourchoice", component: fourchoice },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes: routes,
 });
 
-const app = createApp(App)
+const app = createApp(App);
 app.use(router).use(PrimeVue).mount("#app");
