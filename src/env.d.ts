aca81832c43857f413ed816f6879b8f4c85144e2
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-07-27 21:22:47 +0000

    chore: Declare image asset type

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,17 @@ declare module "*.vue" {
   export default component;
 }
 
+// #region images
+declare module "*.jpeg" {
+  const url: string;
+  export default url;
+}
+declare module "*.png" {
+  const url: string;
+  export default url;
+}
+// #endregion
+
 interface ImportMeta {
   readonly env: any;
 }
