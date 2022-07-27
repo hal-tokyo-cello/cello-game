a9767a132c50d7c8db11d18706d85f4e774ab824
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-07-27 22:36:52 +0000

    fix: Api access helper function

--- src/util/api.ts
+++ src/util/api.ts
@@ -3,35 +3,34 @@
  */
 export const ServerHost = process.env.CELLO_API_SERVER;
 
+const defaultValidator = (res: Response): boolean | string =>
+  res.status != 200 ? res.statusText : true;
+
 /**
  * API問い合わせ用のfetchヘルパー関数。
  * @param endPoint APIエンドのルート
  * @param method リクエストメソッド
  * @param body リクエストのbody部
- * @param respondValidator レスポンスのバリデータ。検証成功の場合はtrue、失敗の場合はfalseか、出力するメッセージをreturn。
+ * @param respondValidator レスポンスのバリデータ。検証成功の場合はtrue、失敗の場合はfalseか、出力するメッセージを返す。
  * @returns APIからのレスポンス
  */
 export const accessApi = <T, U>(
   endPoint: string,
-  method: "GET" | "POST",
-  body: T,
-  respondValidator?: (res: Response) => boolean | string
+  body?: T,
+  method: "GET" | "POST" | "PUT" = "GET",
+  respondValidator = defaultValidator
 ): Promise<U> => {
-  return fetch(`${ServerHost}/api/${endPoint}`, {
-    body: JSON.stringify(body),
+  return fetch(`${ServerHost}/${endPoint}`, {
+    body: body && JSON.stringify(body),
     method: method,
   }).then((res) => {
-    if (respondValidator === undefined) {
-      if (res.status != 200) {
-        return Promise.reject(res.statusText);
-      }
-    } else {
-      const valid = respondValidator(res);
-      if (typeof valid === "string") {
-        return Promise.reject(valid);
-      } else if (!valid) {
-        return Promise.reject(res.statusText);
-      }
+    const valid = respondValidator(res);
+
+    if (typeof valid === "string") {
+      return Promise.reject(valid);
+    }
+    if (!valid) {
+      return Promise.reject(res.statusText);
     }
 
     return res.json();
