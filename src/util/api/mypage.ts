5dc4ac50367840e5844e17f699cf66ebd95df5ad
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-08-08 04:44:22 +0000

    feat: Add update password api request

--- src/util/api/mypage.ts
+++ src/util/api/mypage.ts
@@ -2,6 +2,11 @@ import { accessApi, User, UserCredential } from ".";
 
 type DeleteUserRequest = Pick<UserCredential, "password">;
 
+interface UpdatePasswordRequest {
+  oldPassword: string;
+  newPassword: string;
+}
+
 export const getUser = (user: string) =>
   accessApi<{}, { user: User }>(`users/${user}`);
 
@@ -9,3 +14,6 @@ export const deleteUser = (user: string, req: DeleteUserRequest) =>
   accessApi<DeleteUserRequest, {}>(`users/${user}`, req, "DELETE", {
     "x-mock-response-code": "200",
   });
+
+export const updatePassword = (user: string, req: UpdatePasswordRequest) =>
+  accessApi<UpdatePasswordRequest, {}>(`users/${user}/password`, req, "PUT");
