342e09d1a7300ff70b460e5234de1407dd89e79f
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-08-10 06:24:56 +0000

    feat: Create update username view

---
+++ src/views/MyPage/UpdateUsername.vue
@@ -0,0 +1,92 @@
+<template>
+  <c-form-layout title="ユーザー名の変更" @submit.prevent="confirm">
+    <template #fields>
+      <span class="p-float-label">
+        <p-input-text
+          id="username"
+          v-model="username"
+          autocomplete="username"
+        />
+        <label for="username">ユーザー名</label>
+      </span>
+      <p v-if="isInvalid" class="p-error">このユーザー名は使用できません</p>
+    </template>
+
+    <template #action>
+      <p-button label="キャンセル" @click="cancel" class="p-button-outlined" />
+      <p-button label="確認" type="submit" />
+    </template>
+  </c-form-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from "vue";
+import { RouteRecordRaw } from "vue-router";
+import { ToastSeverity } from "primevue/api";
+
+import { updateUsername, User } from "../../util/api";
+
+import PButton from "primevue/button";
+import PInputText from "primevue/inputtext";
+
+import CFormLayout from "../../layout/Form.vue";
+import { route as MyPage } from "./Mypage.vue";
+import { userKey } from "../../App.vue";
+
+const component = defineComponent({
+  components: {
+    CFormLayout,
+    PButton,
+    PInputText,
+  },
+  data: () => ({
+    username: "",
+    user: inject(userKey) as User,
+  }),
+  computed: {
+    isInvalid() {
+      return this.username.length <= 0;
+    },
+  },
+  methods: {
+    leave() {
+      return this.$router.push({ path: MyPage.path });
+    },
+    cancel() {
+      this.leave();
+    },
+    confirm() {
+      if (this.isInvalid) {
+        return;
+      }
+
+      if (this.username === this.user.name) {
+        this.leave();
+        return;
+      }
+
+      updateUsername(this.user.accountId, { username: this.username }).then(
+        () =>
+          this.leave().then(() =>
+            this.$toast.add({
+              severity: ToastSeverity.SUCCESS,
+              life: 3000,
+              summary: "ユーザー名を更新しました",
+            })
+          ),
+        () =>
+          this.$toast.add({
+            severity: ToastSeverity.ERROR,
+            life: 3000,
+            summary: "ユーザー名を更新できませんでした",
+          })
+      );
+    },
+  },
+});
+
+export const route: RouteRecordRaw = { path: "/mypage/username", component };
+export default component;
+</script>
+
+<style scoped></style>
