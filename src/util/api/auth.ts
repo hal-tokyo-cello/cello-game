09666aa6255b42f67f2ff99f097fe84e664028d7
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-08-20 08:31:09 +0000

    feat: Implement api request for forget password

--- src/util/api/auth.ts
+++ src/util/api/auth.ts
@@ -11,6 +11,10 @@ export type SignInResponse = User | ApiError;
 export type SignUpRequest = UserCredential;
 export type SignUpResponse = {};
 
+export interface ForgetPasswordRequest {
+  email: string;
+}
+
 export const signIn = (req: SignInRequest) =>
   accessApi<SignInRequest, SignInResponse>("users/signin", req, "POST").then(
     (data) => (isApiError(data) ? Promise.reject(data) : data)
@@ -18,3 +22,6 @@ export const signIn = (req: SignInRequest) =>
 
 export const signUp = (req: SignUpRequest) =>
   accessApi<SignUpRequest, SignUpResponse>("users/signup", req, "POST");
+
+export const forget = (req: ForgetPasswordRequest) =>
+  accessApi<ForgetPasswordRequest, {}>("users/forget", req, "POST");
