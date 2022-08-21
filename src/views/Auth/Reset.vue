71359185b83c362e1cffffaa1d1bfee85cbf0cac
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-08-21 08:27:50 +0000

    feat: Install api request

--- src/views/Auth/Reset.vue
+++ src/views/Auth/Reset.vue
@@ -2,7 +2,11 @@
   <c-form-layout title="パスワード再設定" @submit.prevent="Signup">
     <template #fields>
       <span class="p-float-label">
-        <p-password v-model="password" id="password" />
+        <p-password
+          v-model="password"
+          autocomplete="new-password"
+          id="password"
+        />
         <label for="password">
           新規パスワード
           <span style="font-size: 12px">
@@ -10,16 +14,23 @@
           </span>
         </label>
       </span>
-      <p v-if="passFlag" class="p-error">パスワードが入力されていません</p>
-      <p v-if="passwordCheckFlag" class="p-error">
+      <p v-if="isEmptyPassword" class="p-error">
+        パスワードが入力されていません
+      </p>
+      <p v-if="isMismatchPassword" class="p-error">
         確認用パスワードと一致しません
       </p>
 
       <span class="p-float-label">
-        <p-password v-model="confPassword" id="checkPass" :feedback="false" />
+        <p-password
+          v-model="confPassword"
+          :feedback="false"
+          autocomplete="new-password"
+          id="checkPass"
+        />
         <label for="checkPass">確認用パスワード</label>
       </span>
-      <p v-if="confPassFlag" class="p-error">
+      <p v-if="isEmptyConf" class="p-error">
         確認用パスワードが入力されていません
       </p>
     </template>
@@ -30,7 +41,7 @@
     </template>
 
     <template #links>
-      <router-link style="text-decoration: none" to="/signin">
+      <router-link to="/signin">
         <p-button
           label="サインインはこちら"
           class="p-button-link p-button-sm"
@@ -41,13 +52,18 @@
 </template>
 
 <script lang="ts">
+import { ToastSeverity } from "primevue/api";
 import { defineComponent } from "vue";
 import { RouteRecordRaw } from "vue-router";
+
+import { ApiError, reset } from "../../util/api";
 
 import PButton from "primevue/button";
 import PPassword from "primevue/password";
 
 import CFormLayout from "../../layout/Form.vue";
+import { route as SignIn } from "./Signin.vue";
+import { route as Forget } from "./Forget.vue";
 
 const component = defineComponent({
   components: {
@@ -55,48 +71,70 @@ const component = defineComponent({
     PButton,
     PPassword,
   },
-  data() {
-    return {
-      password: "",
-      confPassword: "",
-      mailFlag: false,
-      mailValFlag: false,
-      passFlag: false,
-      confPassFlag: false,
-      passwordCheckFlag: false,
-    };
+  data: () => ({
+    password: "",
+    confPassword: "",
+    attempted: false,
+    request: "",
+  }),
+  computed: {
+    isEmptyPassword() {
+      return this.attempted && this.password.length <= 0;
+    },
+    isEmptyConf() {
+      return this.attempted && this.confPassword.length <= 0;
+    },
+    isMismatchPassword() {
+      return this.attempted && this.password !== this.confPassword;
+    },
   },
   methods: {
     Signup() {
-      var error = false;
-      this.mailFlag = false;
-      this.passFlag = false;
-      this.mailValFlag = false;
-      this.confPassFlag = false;
-      this.passwordCheckFlag = false;
+      this.attempted = true;
 
-      //パスワード入力判定
-      if (this.password.length == 0) {
-        this.passFlag = true;
-        error = true;
-      } else {
-        //確認パスワード入力判定
-        if (this.confPassword.length == 0) {
-          this.confPassFlag = true;
-          error = true;
-        } else {
-          if (this.password == this.confPassword) {
-            //API処理
-            this.$router.push("/");
-            return;
-          } else {
-            this.passwordCheckFlag = true;
-            error = true;
-          }
-        }
+      if (this.isEmptyPassword || this.isEmptyConf || this.isMismatchPassword) {
+        return;
       }
-      return;
+
+      reset({ password: this.password, requestId: this.request }).then(
+        () =>
+          this.$router.push({ path: SignIn.path }).then(() =>
+            this.$toast.add({
+              severity: ToastSeverity.SUCCESS,
+              life: 3000,
+              summary: "パスワード更新しました",
+            })
+          ),
+        (error: ApiError) =>
+          error.error.code.toString(10)[0] == "4" // status code of 4xx
+            ? this.$router.push({ path: Forget.path }).then(() =>
+                this.$toast.add({
+                  severity: ToastSeverity.ERROR,
+                  life: 5000,
+                  summary: "パスワード更新できませんでした",
+                  detail:
+                    "「パスワードを忘れました」からもう一度やり直してください。",
+                })
+              )
+            : this.$toast.add({
+                severity: ToastSeverity.ERROR,
+                life: 5000,
+                summary: "パスワード更新できませんでした",
+              })
+      );
     },
+  },
+  mounted() {
+    if (typeof this.$route.query.requestId !== "string") {
+      this.$toast.add({
+        severity: ToastSeverity.ERROR,
+        life: 5000,
+        summary: "未知のエラーが発生しました",
+        detail: "パスワードリセット用メールからやり直してください。",
+      });
+    } else {
+      this.request = this.$route.query.requestId;
+    }
   },
 });
 
