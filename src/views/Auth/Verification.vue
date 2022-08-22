6879771048226fd9258d65a4e5388ae3c2507003
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-08-22 16:37:21 +0000

    feat: Install verification api request

--- src/views/Auth/Verification.vue
+++ src/views/Auth/Verification.vue
@@ -1,13 +1,13 @@
 <template>
-  <c-form-layout title="ワンタイムパス入力画面">
+  <c-form-layout title="ワンタイムパス入力画面" @submit.prevent="confirm">
     <p style="margin-top: 50px; text-align: center">
       {{ `${email}に4桁の認証コードを送信しました。` }}
     </p>
 
     <template #fields>
       <span class="p-float-label">
-        <p-input-text id="email" type="email" v-model="value" />
-        <label for="email">メール記載の認証コードを入力してください。</label>
+        <p-input-text id="otp" type="number" v-model="otp" />
+        <label for="otp">メール記載の認証コードを入力してください。</label>
       </span>
     </template>
 
@@ -15,7 +15,7 @@
       <router-link :to="SignUp.path" style="text-decoration: none">
         <p-button label="キャンセル" class="p-button-outlined" />
       </router-link>
-      <p-button label="送信" type="submit" />
+      <p-button label="送信" :disabled="!canTry" type="submit" />
     </template>
   </c-form-layout>
 </template>
@@ -30,7 +30,14 @@ import PInputText from "primevue/inputtext";
 import PPassword from "primevue/password";
 
 import CFormLayout from "../../layout/Form.vue";
+import { ApiError, verify } from "../../util/api";
+import { route as SignIn } from "./Signin.vue";
 import { route as SignUp, signUpMailKey } from "./Signup.vue";
+
+const errorDetail: Record<number, string> = {
+  400: "OTPが一致しません。",
+  404: "メールアドレスが登録していません。",
+};
 
 const component = defineComponent({
   components: {
@@ -42,8 +49,30 @@ const component = defineComponent({
   data: () => ({
     SignUp,
     email: "",
-    value: "",
+    otp: "",
+    canTry: true,
   }),
+  methods: {
+    confirm(ev: SubmitEvent) {
+      Promise.resolve(() => {
+        this.canTry = false;
+      })
+        .then(() => verify({ email: this.email, otp: this.otp }))
+        .then(
+          () => this.$router.push({ path: SignIn.path }),
+          (error: ApiError) =>
+            this.$toast.add({
+              severity: ToastSeverity.ERROR,
+              life: 5000,
+              summary: "メールアドレス認証できませんでした",
+              detail: errorDetail[error.error.code],
+            })
+        )
+        .then(() => {
+          this.canTry = true;
+        });
+    },
+  },
   mounted() {
     const mail = localStorage.getItem(signUpMailKey);
     localStorage.removeItem(signUpMailKey);
