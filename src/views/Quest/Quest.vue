7d64543db3fec2a9e71e619209054cf579ce05f5
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-08-24 11:36:01 +0000

    feat: Add switch among quest genres

--- src/views/Quest/Quest.vue
+++ src/views/Quest/Quest.vue
@@ -1,4 +1,7 @@
-<template></template>
+<template>
+  <component v-if="!!quest && !!component" :is="component" :quest="quest" />
+  <p v-else style="text-align: center">このクエストよくわからん。。。</p>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
@@ -6,15 +9,34 @@ import { RouteRecordRaw } from "vue-router";
 
 import { getQuest, QuestSummary } from "../../util/api/quest";
 
+import Combine from "./CombinationQuest.vue";
+import Multiple from "./MultipleChoiceQuest.vue";
+
 /**
  * Name of route param for quest id.
  */
 export const questRouteParam = "quest";
 
 const component = defineComponent({
+  components: {
+    Combine,
+    Multiple,
+  },
   data: () => ({
     quest: {} as QuestSummary,
   }),
+  computed: {
+    component() {
+      switch (this.quest.genre) {
+        case "COM":
+          return Combine;
+        case "MUL":
+          return Multiple;
+        default:
+          return undefined;
+      }
+    },
+  },
   mounted() {
     const questId = this.$route.params[questRouteParam] as string;
     if (typeof questId === "string") {
