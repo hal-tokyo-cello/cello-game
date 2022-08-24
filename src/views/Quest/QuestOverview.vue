1943a31840683362f871fcdeafdcbabe973f0c4a
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-08-24 07:01:13 +0000

    feat: Inline confirm dialog

--- src/views/Quest/QuestOverview.vue
+++ src/views/Quest/QuestOverview.vue
@@ -19,14 +19,37 @@
     </div>
   </div>
 
-  <c-confirm-dialog
-    :modelValue="!!selectedQuest"
-    :quest="selectedQuest"
-    @cancel="selectedQuest = undefined"
-    @confirm="
-      !!selectedQuest && $router.push({ path: `/quests/` + selectedQuest.id })
-    "
-  />
+  <p-dialog
+    modal
+    dismissable-mask
+    close-on-escape
+    :draggable="false"
+    :visible="!!selectedQuest"
+    style="min-width: 620px; text-align: center"
+  >
+    <template #header>
+      <p class="title">{{ selectedQuest?.title }}</p>
+    </template>
+
+    <div>
+      <p class="genre">{{ selectedQuest?.genre }}</p>
+      <p class="experience">{{ selectedQuest?.experience }}</p>
+    </div>
+
+    <template #footer>
+      <div class="actions" style="text-align: center">
+        <p-button label="キャンセル" @click="selectedQuest = undefined" />
+        <p-button
+          label="解答へ"
+          autofocus
+          @click="
+            !!selectedQuest &&
+              $router.push({ path: `/quests/` + selectedQuest.id })
+          "
+        />
+      </div>
+    </template>
+  </p-dialog>
 </template>
 
 <script lang="ts">
@@ -35,11 +58,13 @@ import { RouteRecordRaw } from "vue-router";
 
 import { getQuestSummaryList, QuestSummary } from "../../util/api/quest";
 
-import CConfirmDialog from "../../components/QuestConfirmDialog.vue";
+import PButton from "primevue/button";
+import PDialog from "primevue/dialog";
 
 const component = defineComponent({
   components: {
-    CConfirmDialog,
+    PButton,
+    PDialog,
   },
   data: () => ({
     quests: [] as QuestSummary[],
@@ -123,5 +148,52 @@ export default component;
   font: normal normal bold 16px/27px "Tsukushi A Round Gothic";
   letter-spacing: 0px;
   color: white;
+}
+
+.title,
+.genre,
+.experience {
+  font-weight: bold;
+}
+
+.title {
+  flex: 1 1;
+  font-size: 21px;
+  line-height: 25px;
+  color: #ff8c00;
+}
+
+.genre {
+  font-size: 12px;
+  line-height: 14px;
+  color: #666666;
+}
+
+.genre::before {
+  content: "ジャンル：";
+}
+
+.experience {
+  margin: 0;
+  font-size: 15px;
+  line-height: 18px;
+  letter-spacing: 0px;
+  color: #666666;
+}
+
+.experience::before {
+  content: "🅿︎";
+}
+
+.actions button {
+  width: 150px;
+  height: 52px;
+  margin: 0;
+  cursor: pointer;
+  margin-right: 40px;
+}
+
+.actions button:last-of-type {
+  margin-right: 0;
 }
 </style>
