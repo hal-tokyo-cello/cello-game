db638f9ea2185f068e30d1e12c053e175260b856
KleinChiu <65886108+KleinChiu@users.noreply.github.com>
2022-09-28 22:26:49 +0000

    fix: Registration of views

--- src/views/MyPage/index.ts
+++ src/views/MyPage/index.ts
@@ -1,15 +1,35 @@
 import { RouteRecordRaw } from "vue-router";
 
-import { route as DeleteAccount } from "./DeleteAccount.vue";
-import { route as MyPage } from "./Mypage.vue";
-import { route as UpdateEmail } from "./UpdateEmail.vue";
-import { route as UpdateUsername } from "./UpdateUsername.vue";
-import { route as UpdateUserPassword } from "./UpdateUserPassword.vue";
+import {
+  default as DeleteAccount,
+  route as DeleteAccountRoute,
+} from "./DeleteAccount.vue";
+import { default as MyPage, route as MyPageRoute } from "./Mypage.vue";
+import {
+  default as UpdateEmail,
+  route as UpdateEmailRoute,
+} from "./UpdateEmail.vue";
+import {
+  default as UpdateUsername,
+  route as UpdateUsernameRoute,
+} from "./UpdateUsername.vue";
+import {
+  default as UpdateUserPassword,
+  route as UpdateUserPasswordRoute,
+} from "./UpdateUserPassword.vue";
 
 export const routes: RouteRecordRaw[] = [
+  DeleteAccountRoute,
+  MyPageRoute,
+  UpdateEmailRoute,
+  UpdateUsernameRoute,
+  UpdateUserPasswordRoute,
+];
+
+export const components = {
   DeleteAccount,
   MyPage,
   UpdateEmail,
   UpdateUsername,
   UpdateUserPassword,
-];
+};
